7810d6f3ca2998f4a5d8f3be1c2ea2d822dd38c5
Muhammad Zain <zain.ashiq613@gmail.com>
2026-01-27 23:54:28 +0500

    initial setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from 'react-router';
+import Layout from './pages/layout/Layout';
+import { lazy } from 'react';
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Categories = lazy(() => import('./pages/Categories'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
 function App() {
-  return <>abra</>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="allCategories" element={<Categories />} />
+          {/* Not Found Route */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,5 +1,21 @@
+import { Outlet } from 'react-router';
+import TopBar from '../../components/common/TopBar';
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <section>
+      <div>
+        <TopBar />
+      </div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </section>
+  );
 }
 
 export default Layout;
